98b08b1bd24caacd43673578777cd7eea7858935
pdrotmz <pedrotomaz0210@gmail.com>
2026-08-18 10:05:23 -0300

    feat(test): add an extends to BaseIntegration

--- src/test/java/com/vidalink/healthcare/HealthcareApplicationTests.java
+++ src/test/java/com/vidalink/healthcare/HealthcareApplicationTests.java
@@ -1,10 +1,11 @@
 package com.vidalink.healthcare;
 
+import com.vidalink.healthcare.entity.integration.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HealthcareApplicationTests {
+class HealthcareApplicationTests extends BaseIntegrationTest {
 
 	@Test
 	void contextLoads() {
