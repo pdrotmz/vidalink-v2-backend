8dfebdf975be8567b693768c163cbc5381eb9b4b
pdrotmz <pedrotomaz0210@gmail.com>
2026-08-18 10:14:55 -0300

    feat(marketplace): implement domain layer

---
+++ src/main/java/com/vidalink/healthcare/marketplace/domain/exception/ImageEmptyException.java
@@ -0,0 +1,7 @@
+package com.vidalink.healthcare.marketplace.domain.exception;
+
+public class ImageEmptyException extends RuntimeException {
+    public ImageEmptyException(String message) {
+        super(message);
+    }
+}
