f1f72deb5e52ad4b4948b30bc5e58fcc1992451c
pdrotmz <pedrotomaz0210@gmail.com>
2026-08-24 02:45:23 -0300

    feat(gamification): implement infrastructure layer

---
+++ src/main/java/com/vidalink/healthcare/gamification/infrastructure/persistence/jpa/userbadge/JpaUserBadgeRepository.java
@@ -0,0 +1,15 @@
+package com.vidalink.healthcare.gamification.infrastructure.persistence.jpa.userbadge;
+
+import com.vidalink.healthcare.gamification.entity.domain.userbadge.UserBadge;
+import com.vidalink.healthcare.gamification.entity.enums.badge.Badge;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface JpaUserBadgeRepository extends JpaRepository<UserBadge, UUID> {
+
+    List<UserBadge> findByUserId(UUID userId);
+
+    boolean existsByUserIdAndBadge(UUID userId, Badge badge);
+}
