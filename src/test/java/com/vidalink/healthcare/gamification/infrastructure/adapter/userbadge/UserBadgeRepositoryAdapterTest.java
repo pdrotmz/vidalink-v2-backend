a7b1e8f2dd36e4eb06c7bcfd188f52bc416c56f4
pdrotmz <pedrotomaz0210@gmail.com>
2026-08-24 02:48:43 -0300

    test(gamification): implement gamification infrastructure layer

---
+++ src/test/java/com/vidalink/healthcare/gamification/infrastructure/adapter/userbadge/UserBadgeRepositoryAdapterTest.java
@@ -0,0 +1,73 @@
+package com.vidalink.healthcare.gamification.infrastructure.adapter.userbadge;
+
+import com.vidalink.healthcare.gamification.entity.domain.userbadge.UserBadge;
+import com.vidalink.healthcare.gamification.entity.enums.badge.Badge;
+import com.vidalink.healthcare.gamification.infrastructure.persistence.adapter.userbadge.UserBadgeRepositoryAdapter;
+import com.vidalink.healthcare.gamification.infrastructure.persistence.jpa.userbadge.JpaUserBadgeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UserBadgeRepositoryAdapterTest {
+
+    @Mock
+    private JpaUserBadgeRepository userBadgeRepository;
+
+    @InjectMocks
+    private UserBadgeRepositoryAdapter adapter;
+
+    @Test
+    void shouldSaveUserBadge() {
+        UserBadge userBadge = new UserBadge();
+
+        when(userBadgeRepository.save(userBadge))
+                .thenReturn(userBadge);
+
+        UserBadge result = adapter.save(userBadge);
+
+        assertEquals(userBadge, result);
+
+        verify(userBadgeRepository).save(userBadge);
+    }
+
+    @Test
+    void shouldFindById() {
+        UUID userId = UUID.randomUUID();
+
+        List<UserBadge> userBadge = List.of(new UserBadge());
+
+        when(userBadgeRepository.findByUserId(userId)).thenReturn(userBadge);
+
+        List<UserBadge> result = adapter.findByUserId(userId);
+
+        assertEquals(userBadge, result);
+
+        verify(userBadgeRepository).findByUserId(userId);
+    }
+
+    @Test
+    void shouldReturnTrueWhenExistsByUserIdAndBadge() {
+        UUID userId = UUID.randomUUID();
+        Badge badge = Badge.CONTRIBUTOR;
+
+        when(userBadgeRepository.existsByUserIdAndBadge(userId, badge)).thenReturn(true);
+
+        boolean result = adapter.existsByUserIdAndBadge(userId, badge);
+
+        assertTrue(result);
+
+        verify(userBadgeRepository).existsByUserIdAndBadge(userId, badge);
+    }
+}
