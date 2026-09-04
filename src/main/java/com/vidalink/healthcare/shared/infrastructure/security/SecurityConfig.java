2605eac2a8a9278f7375fa9b7479e5e325ba8c86
pdrotmz <pedrotomaz0210@gmail.com>
2026-09-04 16:18:14 -0300

    prepare to frontend with cors

--- src/main/java/com/vidalink/healthcare/shared/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/vidalink/healthcare/shared/infrastructure/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -35,6 +36,7 @@ public class SecurityConfig {
 
         http.
                 csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .headers(headers ->
                         headers.frameOptions(frame ->
                                 frame.sameOrigin()))
